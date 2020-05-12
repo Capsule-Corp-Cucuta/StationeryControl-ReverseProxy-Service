6c09b59e92670b350b5396b08b0a1302446304f1
Sergio Rodriguez <sergioarodram@gmail.com>
2020-05-12 13:41:01 -0500

    Added CORS properties. Updated service for feign.

--- src/main/java/co/gov/ids/stationerycontrol/zuul/Service.java
+++ src/main/java/co/gov/ids/stationerycontrol/zuul/Service.java
@@ -1,9 +1,13 @@
 package co.gov.ids.stationerycontrol.zuul;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * ReverseProxy Service for stationery control of live births and deaths.
@@ -19,6 +23,24 @@ public class Service {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Service.class).web(true).run(args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }

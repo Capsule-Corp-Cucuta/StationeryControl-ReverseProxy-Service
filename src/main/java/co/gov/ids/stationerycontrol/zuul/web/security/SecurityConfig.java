359f917b30bea69c377c4872a05f00b51080ddf8
Sergio Rodriguez Ramirez <sergioarodram@gmail.com>
2021-01-29 22:53:51 -0500

    update review january 2021

--- src/main/java/co/gov/ids/stationerycontrol/zuul/web/security/SecurityConfig.java
+++ src/main/java/co/gov/ids/stationerycontrol/zuul/web/security/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests().antMatchers("/**/auth").permitAll()
+                .antMatchers("/**/user/{id}/recover-password").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
